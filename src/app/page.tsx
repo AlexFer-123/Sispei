d83039450a5cbf2913b91dc883a5186454329b87
Alexandre <Alexandrefer1231@gmail.com>
2023-10-09 23:11:13 -0300

    auth working

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,8 @@
-import MaxWidthWrapper from "./components/MaxWidthWrapper";
-import Link from "next/link";
+import MaxWidthWrapper from "@/components/MaxWidthWrapper";
+import Image from "next/image";
 import { ArrowRight } from "lucide-react";
 import { buttonVariants } from "@/components/ui/button";
+import { LoginLink, RegisterLink } from "@kinde-oss/kinde-auth-nextjs/server";
 
 export default function Home() {
     return (
@@ -19,27 +20,52 @@ export default function Home() {
 
           <div className="flex">
 
-          <Link className={buttonVariants({
+          <LoginLink className={buttonVariants({
             size: "lg",
             className: "mr-5 mt-5"
-          })} href="/sign-up" target="_blank">
+          })}>
             Entrar <ArrowRight className="ml-2 h-5 w-5" />
-          </Link>
-          <Link className={buttonVariants({
+          </LoginLink>
+          
+          <RegisterLink className={buttonVariants({
             size: "lg",
-            className: "mt-5"
-          })} href="/sign-in" target="_blank">
-            Cadastar <ArrowRight className="ml-2 h-5 w-5" />
-          </Link>
+            className: "mt-5 bg-slate-300 border-blue-600 ring-gray-900/10 ring-1 text-gray-800 hover:text-white position-relative group-hover:group-target:"
+          })}>
+            Cadastar <ArrowRight className="ml-2 h-5 w-5 " />
+          </RegisterLink>
           </div>
         </MaxWidthWrapper>
 
         <div>
           <div className="relative isolate">
-              <div aria-hidden="false" className="pointer-events-none absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80 ">
+              <div aria-hidden="true" className="pointer-events-none absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80 ">
                 <div style={{
-                  clipPath:""
-                }} className="relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]"></div>
+                  clipPath:"polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)"
+                }} className="relative z-0 left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]"></div>
+              </div>
+          </div>
+        </div>
+
+        <div className="mx-auto position-relative z-10 max-w-lg px-6 lg:px-8">
+          <div className="mt-16 flow-root sm:mt-24">
+            <div className="-m-2 rounded-xl bg-gray-900/5 p-2 ring-1 ring-inset ring-gray-900/10 lg:-m-4 lg:rounded-2xl lg:p-4">
+              <Image 
+                src="/modelo-de-doc.png" 
+                alt="preview doc"
+                width={480} 
+                height={720} 
+                className="rounded-md bg-white p-2 sm:p-8 md:p-20 shadow-2xl ring-1 ring-gray-900/10"
+              />
+            </div>
+          </div>
+        </div>
+
+        <div>
+          <div className="relative isolate">
+              <div aria-hidden="true" className="pointer-events-none absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80 ">
+                <div style={{
+                  clipPath:"polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)"
+                }} className="relative z-0 left-[calc(50%-13rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-36rem)] sm:w-[72.1875rem]"></div>
               </div>
           </div>
         </div>
